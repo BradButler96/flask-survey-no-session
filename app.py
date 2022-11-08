b62597634489c1b68a772529b7570d9b5fd537af
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2022-11-07 21:31:41 -0700

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify, url_for
+from flask import Flask, request, render_template, redirect, flash, jsonify, url_for, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
@@ -11,6 +11,7 @@ debug = DebugToolbarExtension(app)
 response = []
 num_of_questions = len(questions)
 
+
 @app.route('/')
 def load_survey():
     # response.clear()
@@ -18,39 +19,35 @@ def load_survey():
 
 @app.route('/question/<int:num>', methods=["GET", "POST"])
 def question(num):
-    if num != len(response) and len(response) < 4:
-        return redirect(url_for('.question', num=len(response)))
-    elif num != len(response) and len(response) == 4:
-        return redirect('/thanks')
+    if num != len(response):
+        if len(response) < 4:
+            return redirect(url_for('.question', num=len(response)))
+        else:
+            return redirect('/thanks')
+    print('line 26')
     print(response)
     return render_template('question.html', survey=satisfaction_survey, questions=questions, num=num, num_of_questions=num_of_questions)
 
 @app.route('/answer/<int:num>')
 def answer(num):
-    choice = request.args.get('choice')
-    if choice == None:
+    session['choice'] = request.args.get('choice')
+    if session['choice'] == None:
         flash('Please answer the question')
         return redirect(url_for('.question', num=num))
     else:
-        new_num = num
-        new_num += 1
-        response.append(choice)
-        print(response)
-        return redirect(url_for('.question', num=new_num))
+        if num < 3:
+            num += 1
+            response.append(session['choice'])
+            return redirect(url_for('.question', num=num))
+        else:
+            response.append(session['choice'])
+            return redirect('/thanks')
+
 
 @app.route('/thanks')
 def thank_you():
-    choice = request.args.get('choice')
-    if choice == None:
-        flash('Please answer the question')
+    print(response)
+    if len(response) < 4:
         return redirect(url_for('.question', num=len(response)))
-    else:
-        response.append(choice)
-        print(response)
-        if len(response) > 4:
-            del response[-1]
-        elif len(response) < 4:
-            return redirect(url_for('.question', num=len(response)))
-        else:
-            return render_template('thanks.html')
+    return render_template('thanks.html')
 
